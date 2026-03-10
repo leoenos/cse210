f130f26ed42a7162164ef45a650700c571a9ae75
leoenos <senos@byupathway.edu>
2026-03-10 00:18:36 -0600

    week 1 exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter grade percent: ");
+        string grade = Console.ReadLine();
+        int finalGrade = int.Parse(grade);
+
+        if (finalGrade >= 90)
+        {
+            Console.Write("Your grade is an A");
+        }
+        else if (finalGrade >= 80 && finalGrade < 90)
+        {
+            Console.Write("Your grade is a B");
+        }
+        else if (finalGrade >= 70 && finalGrade < 80)
+        {
+            Console.Write("Your grade is a C");
+        }
+        else if (finalGrade >= 60 && finalGrade < 70)
+        {
+            Console.Write("Your grade is a D");
+        }
+        else
+        {
+            Console.Write("Your grade is an F");
+        }
+
     }
 }
