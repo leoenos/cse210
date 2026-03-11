2d7e374b1fda2d6dee2cb5e792ecad23d7acbd37
leoenos <senos@byupathway.edu>
2026-03-11 01:18:57 -0600

    week 1 exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        Console.WriteLine("Enter as many numbers as desired. Type 0 when finished.");
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is {sum}");
+
+        double avg = numbers.Average();
+        Console.WriteLine($"The average is {avg}");
+
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is {max}");
+
     }
 }
