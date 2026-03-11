e56f1b021a7c96ec42f74d8721cbb4c2ea2a4aba
leoenos <senos@byupathway.edu>
2026-03-10 23:45:17 -0600

    week 1 exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+        int mNumber = int.Parse(magicNumber);
+
+        Console.Write("What is your guess? ");
+        string yourGuess = Console.ReadLine();
+        int guess = int.Parse(yourGuess);
+        
+        while (guess != mNumber)
+        {
+            if (mNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        }
+
+        Console.Write("You guessed it!");
     }
 }
