e6ac77330159b7f7359d72a648090b8217e2a009
leoenos <senos@byupathway.edu>
2026-03-18 23:28:22 -0600

    week 2 journal start

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,11 +1,24 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("What would you like to do?");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Stats");
+        Console.WriteLine("6. Quit");
 
-        // to do
+        int _input = 0;
+
+        while (_input != 6)
+        {
+            //
+        }
     }
 }
