c2db22c2b3e853d5821079b3deda0f1476fae073
leoenos <senos@byupathway.edu>
2026-04-04 16:10:29 -0600

    address and customer classes finished

--- week04/OnlineOrdering/Address.cs
+++ week04/OnlineOrdering/Address.cs
@@ -2,5 +2,26 @@ using System;
 
 public class Address
 {
-    //
+    private string _street;
+    private string _city;
+    private string _stateOrProvince;
+    private string _country;
+
+    public Address(string street, string city, string stateOrProvince, string country)
+    {
+        _street = street;
+        _city = city;
+        _stateOrProvince = stateOrProvince;
+        _country = country;
+    }
+
+    public bool InUS()
+    {
+        return _country.ToLower() == "united states" || _country.ToLower() == "usa" || _country.ToLower() == "us";
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{_street}\n{_city}, {_stateOrProvince}\n{_country}";
+    }
 }
--- week04/OnlineOrdering/Customer.cs
+++ week04/OnlineOrdering/Customer.cs
@@ -2,5 +2,27 @@ using System;
 
 public class Customer
 {
-    //
+    private string _name;
+    private Address _address;
+
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public Address GetAddress()
+    {
+        return _address;
+    }
+
+    public bool LivesInUS()
+    {
+        return _address.InUS();
+    }
 }
