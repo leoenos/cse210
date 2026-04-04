a81a987a1818f998f76d202db821994eb3588932
leoenos <senos@byupathway.edu>
2026-04-04 14:44:49 -0600

    variables and some getters

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -1,6 +1,24 @@
 using System;
+using System.Reflection.Metadata;
 
 public class Comment
 {
-    //
+    private string _user;
+    private string _text;
+
+    public Comment(string user, string text)
+    {
+        _user = user;
+        _text = text;
+    }
+
+    public string GetUserName()
+    {
+        return _user;
+    }
+
+    public string GetComment()
+    {
+        return _text;
+    }
 }
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -2,5 +2,15 @@ using System;
 
 public class Video
 {
-    //
+    private string _title;
+    private string _author;
+    private int _length;
+    private List<Comment> _comments = new List<Comment>();
+
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+    }
 }
