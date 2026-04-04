7d968f827a4375c080bcec3c30b76d9c991fa5a8
leoenos <senos@byupathway.edu>
2026-04-04 15:59:39 -0600

    order class started, product class done

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -2,5 +2,23 @@ using System;
 
 public class Order
 {
-    //
+    private List<Product> _products = new List<Product>();
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double CalculateTotal()
+    {
+        double total = 0;
+
+        return total;
+    }
 }
