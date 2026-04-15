4f6a64d407f2242d6ff6f4667abfe2e56e973323
leoenos <senos@byupathway.edu>
2026-04-15 17:47:40 -0600

    progrram finish

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Mindfulness Program");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflecting Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Quit");
+
+            Console.Write("Select one: ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                new BreathingActivity().Run();
+            }
+            else if (choice == "2")
+            {
+                new ReflectingActivity().Run();
+            }
+            else if (choice == "3")
+            {
+                new ListingActivity().Run();
+            }
+            else if (choice == "4")
+            {
+                break;
+            }
+        }
     }
 }
