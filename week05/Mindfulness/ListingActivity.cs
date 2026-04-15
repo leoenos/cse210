76aacec5fe9ec2d3ad56297621183a4e4bb6c075
leoenos <senos@byupathway.edu>
2026-04-15 01:08:14 -0600

    mindfullness start

---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,21 @@
+public class ListingActivity
+{
+    private int _count;
+    private List<string> _prompts = new List<string>();
+
+    public ListingActivity()
+    {
+        //
+    }
+
+    public void Run()
+    {
+        //
+    }
+
+    public void GetRandomPrompt()
+    {
+        //
+    }
+
+}
---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,5 @@
+public class ReflectingActivity
+{
+    private List<string> _prompts = new List<string>();
+    private List<string> _questions = new List<string>();
+}
