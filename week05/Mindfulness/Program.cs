c50e6ad83c0dbf0e09ef3c261167c2a228cc3905
leoenos <senos@byupathway.edu>
2026-04-15 22:06:04 -0600

    Final changes and creative element

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,17 +1,21 @@
 using System;
-
+//Added a tracking counter to let the user know how many times they have done each activity.
 class Program
 {
     static void Main(string[] args)
     {
+        int breathingCount = 0;
+        int reflectCount = 0;
+        int listingCount = 0;
+
         while (true)
         {
-            Console.Clear();
             Console.WriteLine("Mindfulness Program");
             Console.WriteLine("1. Breathing Activity");
             Console.WriteLine("2. Reflecting Activity");
             Console.WriteLine("3. Listing Activity");
-            Console.WriteLine("4. Quit");
+            Console.WriteLine("4. Stats");
+            Console.WriteLine("5. Quit");
 
             Console.Write("Select one: ");
             string choice = Console.ReadLine();
@@ -19,16 +23,26 @@ class Program
             if (choice == "1")
             {
                 new BreathingActivity().Run();
+                breathingCount++;
             }
             else if (choice == "2")
             {
                 new ReflectingActivity().Run();
+                reflectCount++;
             }
             else if (choice == "3")
             {
                 new ListingActivity().Run();
+                listingCount++;
             }
             else if (choice == "4")
+            {
+                Console.WriteLine("\nActivity Stats:");
+                Console.WriteLine($"Breathing Activity: {breathingCount}");
+                Console.WriteLine($"Refelctin Activity: {reflectCount}");
+                Console.WriteLine($"Listing Activity: {listingCount}");
+            }
+            else if (choice == "5")
             {
                 break;
             }
