58348adddf7d0d7828b9fd3cf5a928504065ae4a
leoenos <senos@byupathway.edu>
2026-04-16 05:59:02 -0600

    running class

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.Contracts;
+
 public abstract class Activity
 {
     private DateTime _date;
@@ -7,5 +9,25 @@ public abstract class Activity
     {
         _date = date;
         _minutes = minutes;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    public string GetDate()
+    {
+        return _date.ToString("dd MM yyyy");
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        string _summary = $"{GetDate()} {GetType().Name} ({_minutes} min)- Distance {GetDistance():0.0} miles, Speed {GetSpeed():0.0} mph, Pace {GetPace():0.0} min per mile";
+        return _summary;
     }
 }
