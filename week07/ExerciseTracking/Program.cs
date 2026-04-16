dea4c8da42d8e87277085c3b54abee462ee8c38c
leoenos <senos@byupathway.edu>
2026-04-16 06:15:54 -0600

    classes and program implementation

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running(new DateTime(2020, 12, 5), 30, 2.0));
+        activities.Add(new Cycling(new DateTime(2021, 6, 13), 25, 4.2));
+        activities.Add(new Swimming(new DateTime(2022, 11, 3), 30, 40));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
